9af37b6235dbadb0691d69af931b066d58bd82ab
Ryan N <ryguyn@gmail.com>
2016-11-02 21:31:05 -0700

    Made simple button-pushing code that uses the color sensors. Also tested the legacy color sensor, which didn't work.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LegacySensorTest.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+
+@Autonomous(name = "Legacy Sensor Test", group = "Testing")
+public class LegacySensorTest extends OpMode {//CDI stands for Core Device Interface
+    //    private OpticalDistanceSensor ods;
+//    private LightSensor lightSensor;
+    private ColorSensor colorSensor;
+
+
+    @Override
+    public void init() {
+        colorSensor = hardwareMap.colorSensor.get("colorSensor");
+        colorSensor.enableLed(true);
+    }
+
+    @Override
+    public void loop() {
+        telemetry.addData("Red", colorSensor.red());
+        telemetry.addData("Green", colorSensor.green());
+        telemetry.addData("Blue", colorSensor.blue());
+    }
+}
